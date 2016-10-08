8a975f405cc8a2b9eeae8bce53ec7be505a87bf0
Chris Norwood <cnorwood92@gmail.com>
2016-10-07 19:04:59 -0700

    Basic log in log out created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
 
   # Validations
   validates :name,  presence: true, length: { maximum: 50 }
@@ -7,4 +8,41 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+  # Callbacks
+  before_save :downcase_email
+
+  # Remembers the user in the database, for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Forget a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Returns true if rememeber token matches digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Encrypts given token for use in digest
+  def User.digest
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(token, cost: cost)
+  end
+
+  # Generates a new, random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  private
+
+    def downcase_email
+      email.downcase!
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'users#new'
 
-  get  '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
-  get  '/users',  to: 'users#show'
+  get    '/users',  to: 'users#show'
+
+  get    '/signup', to: 'users#new'
+  post   '/signup', to: 'users#create'
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
